9738228d89300b20dde703ec722e2b8b40584978
Colin Chisholm <chizjr@gmail.com>
2021-01-06 23:27:45 -0800

    Working warzone page

--- MW_PROJECT/MW_STATS/views.py
+++ MW_PROJECT/MW_STATS/views.py
@@ -7,6 +7,7 @@ import requests
 from .helpers import *
 from .forms import LoginForm, RegisterForm, FriendsForm
 from .models import Friends, User, Profile
+from time import sleep
 
 
 def index(request):
@@ -117,13 +118,55 @@ def warzone(request):
     top10 = add_commas(wz_stats['br']['topTen'])
     top25 = add_commas(wz_stats['br']['topTwentyFive'])
     revives = add_commas(wz_stats['br']['revives'])
+    sleep(1)
 
     #############################################################################
     # All same info above but for all friends added for that user to compare with
+    friends = Friends.objects.filter(user=request.user).first()
+    friends_list = [friends.gamertag1, friends.gamertag2, friends.gamertag3, friends.gamertag4, friends.gamertag5]
+    # get the API response for warzone stats for the friends of the user
+    wz_stats_F = []
+    for friend in friends_list:
+        stats_list =[]
+        # if there is a gamertag than get the stats, otherwise continue
+        if friend != '':
+            url_friends = "https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/{}/xbl".format(friend)
+            headers = {
+            'x-rapidapi-key': "206fdfeafcmsh70f2e07b4f4d6e0p136146jsn19dcf2a2f09e",
+            'x-rapidapi-host': "call-of-duty-modern-warfare.p.rapidapi.com"
+            }
 
+            # response for the friends - already have headers from above
+            response_friends = requests.request('GET', url_friends, headers=headers)
+            wz_stats_friends = response_friends.json()
 
+            # all warzone stats for friends
+            kd_f = round(wz_stats_friends['br']['kdRatio'], 3)
+            downs_f = add_commas(wz_stats_friends['br']['downs'])
+            top25_f = add_commas(wz_stats_friends['br']['topTwentyFive'])
+            kills_f = add_commas(wz_stats_friends['br']['kills'])
+            deaths_f = add_commas(wz_stats_friends['br']['deaths'])
+            games_played_f = add_commas(wz_stats_friends['br']['gamesPlayed'])
+            wins_f = add_commas(wz_stats_friends['br']['wins'])
+            win_percentage_f = round(wz_stats_friends['br']['wins'] / wz_stats['br']['gamesPlayed'] * 100, 2)
+            top5_percentage_f = round(wz_stats_friends['br']['topFive'] / wz_stats['br']['gamesPlayed'] * 100, 2)
+            top5_f = add_commas(wz_stats_friends['br']['topFive'])
+            top10_f = add_commas(wz_stats_friends['br']['topTen'])
+            top25_f = add_commas(wz_stats_friends['br']['topTwentyFive'])
+            revives_f = add_commas(wz_stats_friends['br']['revives'])
 
-
+            # gather into a list
+            stats_list.extend([
+                friend, win_percentage_f, top5_percentage_f, 
+                wins_f, top5_f, top10_f, top25_f, kills_f,
+                deaths_f, kd_f, revives_f, games_played_f
+                ]
+            )
+            wz_stats_F.append(stats_list)
+            sleep(1) # Sleep for 1 second to avoid API calls/sec error
+        else:
+            continue
+        
     return render(request, 'mw_stats/warzone.html', {
         'gamertag': gamertag,
         'kd': kd,
@@ -138,7 +181,8 @@ def warzone(request):
         'top5': top5,
         'top10': top10,
         'top25': top25,
-        'revives': revives
+        'revives': revives,
+        'wz_stats_F': wz_stats_F
     })
 
 
@@ -147,14 +191,16 @@ def friends(request):
     if request.method == 'POST':
         form = FriendsForm(request.POST)
         if form.is_valid():
-            friends = Friends()
-            friends.user = request.user
-            friends.gamertag1 = form.cleaned_data['gamertag1']
-            friends.gamertag2 = form.cleaned_data['gamertag2']
-            friends.gamertag3 = form.cleaned_data['gamertag3']
-            friends.gamertag4 = form.cleaned_data['gamertag4']
-            friends.gamertag5 = form.cleaned_data['gamertag5']
-            friends.save()
+            Friends.objects.update_or_create(
+                user=request.user,
+                defaults={
+                    'gamertag1': form.cleaned_data['gamertag1'],
+                    'gamertag2': form.cleaned_data['gamertag2'],
+                    'gamertag3': form.cleaned_data['gamertag3'],
+                    'gamertag4': form.cleaned_data['gamertag4'],
+                    'gamertag5': form.cleaned_data['gamertag5']
+                }
+            )
             return HttpResponseRedirect(reverse('warzone'))
         else:
             return render(request, 'mw_stats/friends.html', {
